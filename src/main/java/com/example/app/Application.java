ca0d271f3bf9a89d3f26bcab38852c7f9f2b7fb7
jaime.quispe <jaime.quispe@despegar.com>
2020-03-24 12:58:49 -0300

    Mas ajustes

--- src/main/java/com/example/app/Application.java
+++ src/main/java/com/example/app/Application.java
@@ -1,13 +1,13 @@
-package com.example.weather;
+package com.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeatherApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WeatherApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
